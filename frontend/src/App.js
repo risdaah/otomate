eea79c353beec88d05da032d3945259156b09a67
risdaah <radrisaaa@gmail.com>
2025-05-30 18:20:33 +0700

    create: workplace backend dan frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000')
+      .then(response => {
+        setMessage(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
